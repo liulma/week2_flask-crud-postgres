c3e67b97a96cf009276892e199c4fa84ad817a23
liulma <linda.ulma@skillio.ai>
2025-01-23 13:20:07 +0200

    REST API exercises

--- src/data/attributes_api.py
+++ src/data/attributes_api.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from attributes_service import db_get_attributes, db_get_attribute_by_id
+from attributes_service import db_get_attributes, db_get_attribute_by_id, db_create_attribute, db_update_attribute
 
 app = Flask(__name__)
 
@@ -19,7 +19,33 @@ def get_attribute_by_id(id):
     try:
         return db_get_attribute_by_id(id)
     except:
-        return {"error": "no person with id %s" % id}
+        return {"error": "no attribute with id %s" % id}
+    
+@app.route("/attributes", methods=['POST'])
+def create_attribute():
+    try: 
+        data = request.get_json()
+        attribute_name = data['attribute_name']
+        attribute_description = data['attribute_description']
+        attribute_value = data['attribute_value']
+        person_id = data['person_id']
+        db_create_attribute(attribute_name, attribute_description, attribute_value, person_id)
+        return {"success": "created attribute: %s" % attribute_name}
+    except:
+        return {"error": "error creating attribute"}
+    
+@app.route("/attributes/<int:id>", methods=['PUT'])
+def update_attribute(id):
+    try:
+        data = request.get_json()
+        attribute_name = data['attribute_name']
+        attribute_description = data['attribute_description']
+        attribute_value = data['attribute_value']
+        person_id = data['person_id']
+        db_update_attribute(id, attribute_name, attribute_description, attribute_value, person_id)
+        return {"success": "updated person"}
+    except:
+        return {"error": "error updating person"}
     
 if __name__ == "__main__":
     app.run()
--- src/data/attributes_service.py
+++ src/data/attributes_service.py
@@ -34,3 +34,37 @@ def db_get_attribute_by_id(id):
     finally:
         if con is not None:
             con.close()
+
+def db_create_attribute(attribute_name, attribute_description, attribute_value, person_id):
+    con = None
+    try:
+        con = psycopg2.connect(**config())
+        cursor = con.cursor(cursor_factory=RealDictCursor)
+        SQL = 'INSERT INTO attributes (attribute_name, attribute_description, attribute_value, person_id) VALUES (%s, %s, %s, %s);'
+        cursor.execute(SQL, (attribute_name, attribute_description, attribute_value, person_id))
+        con.commit()
+        result = {"success": "created attribute name: %s " % attribute_name}
+        cursor.close()
+        return json.dumps(result)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if con is not None:
+            con.close()
+
+def db_update_attribute(id, attribute_name, attribute_description, attribute_value, person_id):
+    con = None
+    try:
+        con = psycopg2.connect(**config())
+        cursor = con.cursor(cursor_factory=RealDictCursor)
+        SQL = 'UPDATE attributes SET attribute_name = %s, attribute_description = %s, attribute_value = %s, person_id = %s WHERE id = %s;'
+        cursor.execute(SQL, (attribute_name, attribute_description, attribute_value, person_id, id))
+        con.commit()
+        cursor.close()
+        result = {"success": "updated attribute id: %s " % id}
+        return json.dumps(result)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if con is not None:
+            con.close()
